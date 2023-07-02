37c8ddd9ff55e4619f7d198b5bddf2fb7414b2fa
Aqsa Ashfaq <90766914+AqsaAshfaq07@users.noreply.github.com>
2023-07-02 23:51:21 +0500

    Love Calculator

---
+++ Random Love Calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import random as r
+
+name = input("Enter your name:  ")
+partner = input("Enter your partner name: ")
+
+love_score = r.randint(1, 100)
+print(f"Your love score is {love_score}")
+
